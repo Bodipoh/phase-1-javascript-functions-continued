24edc21d1e511c70faac0837f5de890abc0de551
+ <+>
2022-08-21 12:20:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(){
+return "This saturday, I want to read!";
+}
+
+function mondayWork (){
+    return "This Monday I will go to the office.";
+}
+
+
+function wrapAdjective (){
+    let result = wrapAdjective('*');
+    let emphatic = result("a hard worker");
+}
